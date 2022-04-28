3d51a26329c596103f0e91669948469756e7b443
Lucasgrn <dev01@aeoluscloud.com>
2022-04-28 10:33:25 -0300

    Limpando o código

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-from platform import platform
 import pygame
 from player import Player
 pygame.init()
@@ -9,28 +8,18 @@ pygame.display.set_caption('Wave Slayer') #Nome do jogo (podem mudar!)
 drawGroup = pygame.sprite.Group()
 player = Player(drawGroup)
 
-def draw():
+def draw(): #Renderiza os objetos na tela 
     display.fill([12, 102, 15])
     drawGroup.draw(display)
     
 
 gameloop = True
-#pressW = False
 
 while gameloop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameloop = False
-
-        # elif event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_w:
-        #         pressW = True
-        #         print('apertou')
-        # elif event.type == pygame.KEYUP:
-        #     if event.key == pygame.K_w:
-        #         pressW = False
-        #         print('soltou')
-
+        
     draw()
-    drawGroup.update()
+    drawGroup.update() #Aplica movimentação
     pygame.display.update()
--- player.py
+++ player.py
@@ -6,16 +6,19 @@ class Player(pygame.sprite.Sprite):
 
         self.image = pygame.image.load('assets/player.png')
         self.image = pygame.transform.scale(self.image, [50, 50])
-        self.rect = pygame.Rect(50, 50, 50, 50)
+        self.rect = pygame.Rect(640, 360, 50, 50)
 
     def update(self, *args):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
             self.rect.y -= 1
+
         elif keys[pygame.K_s]:
             self.rect.y += 1
+
         elif keys[pygame.K_a]:
             self.rect.x -= 1
+
         elif keys[pygame.K_d]:
             self.rect.x += 1
