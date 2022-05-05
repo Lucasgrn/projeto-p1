2b26eb0151748fffb581a6025242b7032352d300
Lucasgrn <dev01@aeoluscloud.com>
2022-05-05 14:04:42 -0300

    Gerando novos inimigos

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import random
 import pygame
+from bullet import BulletDown, BulletRight, BulletUp
 from player import Player
-from enemy import Enemy
+from enemy import EnemyToLeft, EnemyToUp, EnemyToDown
 pygame.init()
 
 display = pygame.display.set_mode([1280, 720]) #Tamanho da janela
@@ -9,8 +10,6 @@ pygame.display.set_caption('Wave Slayer') #Nome do jogo (podem mudar!)
 
 drawGroup = pygame.sprite.Group()
 player = Player(drawGroup)
-for i in range(20):
-    enemy = Enemy(drawGroup)
 
 def draw(): #Renderiza os objetos na tela 
     display.fill([12, 102, 15])
@@ -26,14 +25,30 @@ while gameloop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameloop = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT :
+                newBullet = BulletRight(drawGroup)
+                newBullet.rect.center = player.rect.center
+            elif event.key == pygame.K_UP:
+                newBullet = BulletUp(drawGroup)
+                newBullet.rect.center = player.rect.center
+            elif event.key == pygame.K_DOWN:
+                newBullet = BulletDown(drawGroup)
+                newBullet.rect.center = player.rect.center
+
+
         
     draw()
     drawGroup.update() #Aplica movimentação 
 
     #Gerando Inimigos
     timer += 1
-    if timer == 4:
+    if timer == 15:
         timer = 0
-        newEnemy = Enemy(drawGroup)
+        newEnemyLeft = EnemyToLeft(drawGroup)
+        newEnemyUp = EnemyToUp(drawGroup)
+        newEnemyDown = EnemyToDown(drawGroup)
+        
+
 
     pygame.display.update()
