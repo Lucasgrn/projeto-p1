003ac1a35a3b980d27e4d66abb3ca86babb60685
Lucasgrn <dev01@aeoluscloud.com>
2022-05-04 22:57:56 -0300

    Geração do inimigo

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
+import random
 import pygame
 from player import Player
-#from enemy import Enemy
+from enemy import Enemy
 pygame.init()
 
 display = pygame.display.set_mode([1280, 720]) #Tamanho da janela
@@ -8,6 +9,8 @@ pygame.display.set_caption('Wave Slayer') #Nome do jogo (podem mudar!)
 
 drawGroup = pygame.sprite.Group()
 player = Player(drawGroup)
+for i in range(20):
+    enemy = Enemy(drawGroup)
 
 def draw(): #Renderiza os objetos na tela 
     display.fill([12, 102, 15])
@@ -15,6 +18,7 @@ def draw(): #Renderiza os objetos na tela
     
 
 gameloop = True
+timer = 0
 clock = pygame.time.Clock()
 
 while gameloop:
@@ -24,5 +28,12 @@ while gameloop:
             gameloop = False
         
     draw()
-    drawGroup.update() #Aplica movimentação
+    drawGroup.update() #Aplica movimentação 
+
+    #Gerando Inimigos
+    timer += 1
+    if timer == 4:
+        timer = 0
+        newEnemy = Enemy(drawGroup)
+
     pygame.display.update()
