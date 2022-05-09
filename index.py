de88a54d1433abffaee8245564f7527385df6354
Lucasgrn <dev01@aeoluscloud.com>
2022-05-09 18:13:21 -0300

    Ajustes importantes

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-import random
 import pygame
 from bullet import BulletDown, BulletLeft, BulletRight, BulletUp
 from player import Player
@@ -9,11 +8,15 @@ display = pygame.display.set_mode([1280, 720]) #Tamanho da janela
 pygame.display.set_caption('Wave Slayer') #Nome do jogo (podem mudar!)
 
 drawGroup = pygame.sprite.Group()
+enemyGroup = pygame.sprite.Group()
+bulletGroup = pygame.sprite.Group()
 player = Player(drawGroup)
 
 def draw(): #Renderiza os objetos na tela 
     display.fill([12, 102, 15])
     drawGroup.draw(display)
+    enemyGroup.draw(display)
+    bulletGroup.draw(display)
     
 
 gameloop = True
@@ -27,32 +30,39 @@ while gameloop:
             gameloop = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT :
-                newBullet = BulletRight(drawGroup)
+                newBullet = BulletRight(bulletGroup)
                 newBullet.rect.center = player.rect.center
             elif event.key == pygame.K_LEFT:
-                newBullet = BulletLeft(drawGroup)
+                newBullet = BulletLeft(bulletGroup)
                 newBullet.rect.center = player.rect.center
             elif event.key == pygame.K_UP:
-                newBullet = BulletUp(drawGroup)
+                newBullet = BulletUp(bulletGroup)
                 newBullet.rect.center = player.rect.center
             elif event.key == pygame.K_DOWN:
-                newBullet = BulletDown(drawGroup)
+                newBullet = BulletDown(bulletGroup)
                 newBullet.rect.center = player.rect.center
 
 
         
     draw()
     drawGroup.update() #Aplica movimentação 
+    enemyGroup.update()
+    bulletGroup.update()
 
     #Gerando Inimigos
     timer += 1
-    if timer == 15:
+    if timer == 30:
         timer = 0
-        newEnemyLeft = EnemyToLeft(drawGroup)
-        newEnemyUp = EnemyToUp(drawGroup)
-        newEnemyDown = EnemyToDown(drawGroup)
-        newEnemyRight = EnemyToRight(drawGroup)
-        
+        newEnemyLeft = EnemyToLeft(enemyGroup)
+        newEnemyUp = EnemyToUp(enemyGroup)
+        newEnemyDown = EnemyToDown(enemyGroup)
+        newEnemyRight = EnemyToRight(enemyGroup)
+    
+    collide = pygame.sprite.spritecollide(player, enemyGroup, False)
+    killEnemy = pygame.sprite.groupcollide(bulletGroup, enemyGroup, True, True)
 
+    if collide:
+        print('Fim de jogo')
+        gameloop = False
 
     pygame.display.update()
